ccd5ee472243bf2e99ff6c0209d92ecda7b56d02
Cyrano75 <berthierjg@gmail.com>
2023-11-21 21:53:22 +0100

    use api view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('realstates/<int:pk>/', views.realestate_detail),
-    path('realstates/', views.realestate_list)
+    path('realstates/<int:pk>/', views.RealEstateDetail.as_view()),
+    path('realstates/', views.RealEstateList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,38 +1,44 @@
-from rest_framework.parsers import JSONParser
 from .models import RealEstateSerializer
-from django.http import HttpResponse, JsonResponse
 from .models import RealEstate
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-@csrf_exempt
-def realestate_list(request):
-    if request.method == 'GET':
+class RealEstateList(APIView):
+    def get(self, request, format=None):
         realestates = RealEstate.objects.all()
         serializer = RealEstateSerializer(realestates, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = RealEstateSerializer(data=data)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = RealEstateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    
-@csrf_exempt
-def realestate_detail(request, pk):
-    try:
-        realestate = RealEstate.objects.get(pk=pk)
-    except RealEstate.DoesNotExist:
-        return HttpResponse(status=404)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'GET':
+class RealEstateDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return RealEstate.objects.get(pk=pk)
+        except RealEstate.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        realestate = self.get_object(pk)
         serializer = RealEstateSerializer(realestate)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = RealEstateSerializer(realestate, data=data)
+    def put(self, request, pk, format=None):
+        realestate = self.get_object(pk)
+        serializer = RealEstateSerializer(realestate, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
